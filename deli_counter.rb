3f9f2f76c72ec242034b1bc050321a79695993a2
Zachary <zbberman@yahoo.com>
2019-01-30 17:58:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,10 @@ def take_a_number(line_array, name)
 end
 
 def now_serving(line_array)
-  
+  if line_array.size == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{line_array.shift}."
 end 
 
 
