f3e7dc76bec1f515aa22bf92149622977db9d6c6
Zachary <zbberman@yahoo.com>
2019-01-30 17:54:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,35 @@ def take_a_number(line_array, name)
   
 end
 
+describe "#take_a_number" do
+    context "there is nobody in line" do
+      it "should add a person to the line" do
+        expect($stdout).to receive(:puts).with("Welcome, Ada. You are number 1 in line.")
+        take_a_number(katz_deli, "Ada")
+        expect(katz_deli).to eq(["Ada"])
+      end
+    end
+
+    context "there are already people in line" do
+      it "should add a person to the end of the line" do
+        # This weird looking %w syntax is just another way to instantiate an
+        # Array. %w(item1 item2) is equivalent to ["item1", "item2"]
+        expect($stdout).to receive(:puts).with("Welcome, Grace. You are number 4 in line.")
+        take_a_number(other_deli, "Grace")
+        expect(other_deli).to eq(%w(Logan Avi Spencer Grace))
+      end
+    end
+
+    context "adding multiple people in a row" do
+      it "should correctly build the line" do
+        take_a_number(katz_deli, "Ada")
+        take_a_number(katz_deli, "Grace")
+        take_a_number(katz_deli, "Kent")
+        expect(katz_deli).to eq(%w(Ada Grace Kent))
+      end
+    end
+  end
+
 def now_serving(line_array)
   
 end 
